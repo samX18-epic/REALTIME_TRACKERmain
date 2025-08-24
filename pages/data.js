cb875992d154fba0eae3498fe9a72d42669b2d17
Sambhab Roy <samspc@Sambhab-ka-MacBook-Air.local>
2025-08-24 16:23:38 +0530

    final remove

--- pages/data.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  // Set CORS headers to allow requests from other origins
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  // Handle the API request and send a response
-  res.status(200).json({ message: 'CORS is enabled for this route!' });
-}
